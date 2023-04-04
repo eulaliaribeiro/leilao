b769bf32f48fe31015ec1fea99e83bdbf82abdf1
Eulália Ribeiro <eulaliaribeiroecr@gmail.com>
2023-04-03 23:51:22 -0300

    Initial commit

---
+++ src/test/java/com/deva/testes/leilao/LeilaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deva.testes.leilao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeilaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
