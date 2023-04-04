4c4df839127b5c93bd88028704a57e337c57fa22
Eulália Ribeiro <eulaliaribeiroecr@gmail.com>
2023-04-04 05:53:16 -0300

    ajuste classe Leilao

--- src/main/java/model/Leilao.java
+++ src/main/java/model/Leilao.java
@@ -1,20 +1,3 @@
-/*
-0 - Utilizar Junit 5 para todos os testes
-
-1 - Criar 2 classes service
-
-2 - Criar 1 entidade
-
-3 - Criar 6 métodos
-
-4 - Possuir 90% de coverage (services)
-
-5 - Criar 1 teste de exception
-
-6 - Utilizar mocks
-
-7 - Adicionar o Jacoco ao projeto
-* */
 package model;
 
 import java.time.LocalDate;
